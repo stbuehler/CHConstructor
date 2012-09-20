3ef6d0cc3bf815f6905f83b42a47439e51c3491d
Niklas Schnelle <niklas@komani.de>
2012-09-20 20:59:55 +0200

    Options to allow text-input and output+cleanups

--- src/main/java/de/tourenplaner/chconstruction/Main.java
+++ src/main/java/de/tourenplaner/chconstruction/Main.java
@@ -8,6 +8,75 @@ import java.util.Random;
 
 public class Main {
 
+    private static void preCHBenchTest(RAMGraph graph, RAMGraph prunedGraph){
+        Dijkstra myDijkstra = new Dijkstra(graph);
+        BDDijkstra myBDDijkstra = new BDDijkstra(prunedGraph);
+        Random generator = new Random();
+        generator.setSeed(42);
+        int src = generator.nextInt(graph.nofNodes());
+        int trg = generator.nextInt(graph.nofNodes());
+        for (int i = 0; i < 2; i++) {
+            long curTime = System.currentTimeMillis();
+            int dist = myDijkstra.runDijkstra(src, trg);
+            long timeDelta = System.currentTimeMillis() - curTime;
+            System.out.println("Distance from " + src + " to " + trg + " is " + dist + " in time " + timeDelta);
+
+            curTime = System.currentTimeMillis();
+            int dist2 = myBDDijkstra.runDijkstra(src, trg);
+            timeDelta = System.currentTimeMillis() - curTime;
+            System.out.println("BDDistance from " + src + " to " + trg + " is " + dist2 + " in time " + timeDelta);
+            System.out.println();
+
+            // myDijkstra.printGeoPath(trg);
+            assert (dist == dist2);
+
+            src = generator.nextInt(graph.nofNodes());
+            trg = generator.nextInt(graph.nofNodes());
+        }
+    }
+
+    private static void withChBench(RAMGraph prunedGraph, RAMGraph graphCH) {
+        long curTime;
+        long timeDelta;
+        Random generator = new Random();
+        generator.setSeed(42);
+        int src = generator.nextInt(prunedGraph.nofNodes());
+        int trg = generator.nextInt(prunedGraph.nofNodes());
+        CHDijkstra myCHDijkstra = new CHDijkstra(graphCH);
+        myCHDijkstra.checkCHreqs();
+        Dijkstra myDijkstra = new Dijkstra(prunedGraph);
+        int minDist = Integer.MAX_VALUE;
+
+        for (int i = 0; i < 100; i++) {
+            //if ((graphCH.level(src)>0)&&(graphCH.level(trg)>0))
+            {
+                curTime = System.nanoTime();
+                int dist = myDijkstra.runDijkstra(graphCH.altNodeID(src), graphCH.altNodeID(trg));
+                timeDelta = (System.nanoTime() - curTime) / 1000;
+
+
+                curTime = System.nanoTime();
+                int dist2 = myCHDijkstra.runDijkstra(src, trg);
+                long timeDelta2 = (System.nanoTime() - curTime) / 1000;
+                {
+                    minDist = dist;
+                    System.out.println("Distance from " + graphCH.altNodeID(src) + " to " + graphCH.altNodeID(trg) + " is " + dist + " in time " + timeDelta);
+                    System.out.println("CH-Distance in CH from " + src + " to " + trg + " is " + dist2 + " in time " + timeDelta2);
+                    System.out.println("Levels are " + graphCH.level(src) + "/" + graphCH.level(trg));
+                    if (dist == Integer.MAX_VALUE)
+                        System.out.println("*******************************************************");
+                    // myDijkstra.printPath(trg);
+                    System.out.println();
+                    assert (dist == dist2);
+                }
+
+                src = generator.nextInt(prunedGraph.nofNodes());
+                trg = generator.nextInt(prunedGraph.nofNodes());
+            }
+
+        }
+    }
+
 	/**
 	 * @param args
 	 */
@@ -15,7 +84,8 @@ public class Main {
         CommandLineParser parser = new GnuParser();
         Options options = new Options();
 
-        options.addOption("t","text", false, "Use text mode graph file reading for human readable graph data");
+        options.addOption("ti","text-input", false, "Read input graph in text mode");
+        options.addOption("to", "text-output", false, "Write output graph in text mode");
         options.addOption("i", "input-file", true, "The graph file to read from, use - for standard input");
         options.addOption("o", "output-file", true, "The graph file to write the result to, use - for standard output");
         options.addOption("h", "help", false, "Show this help message");
@@ -41,7 +111,7 @@ public class Main {
            }
 
             RAMGraph ramGraph=new RAMGraph();
-            if (cmd.hasOption('t')){
+            if (cmd.hasOption("ti")){
                 ramGraph.readGTXT(istream);
             } else {
                 ramGraph.readBIN(istream);
@@ -50,41 +120,16 @@ public class Main {
             ramGraph.sanityCheck();
 
 
-            RAMGraph ramGraph2=ramGraph.pruneGraph();
+            RAMGraph prunedGraph=ramGraph.pruneGraph();
+            preCHBenchTest(ramGraph, prunedGraph);
+            ramGraph=prunedGraph;
 
-            Dijkstra myDijkstra=new Dijkstra(ramGraph);
-            BDDijkstra myBDDijkstra=new BDDijkstra(ramGraph2);
-
-
-            int src=11, trg=300000;
-            Random generator=new Random();
-            for(int i=0; i<2; i++)
-            {
-                long curTime=System.currentTimeMillis();
-                int dist=myDijkstra.runDijkstra(src,trg);
-                long timeDelta=System.currentTimeMillis()-curTime;
-                System.out.println("Distance from "+src+" to "+trg+" is "+dist+" in time "+timeDelta);
-
-                curTime=System.currentTimeMillis();
-                int dist2=myBDDijkstra.runDijkstra(src, trg);
-                timeDelta=System.currentTimeMillis()-curTime;
-                System.out.println("BDDistance from "+src+" to "+trg+" is "+dist2+" in time "+timeDelta);
-                System.out.println();
-
-                // myDijkstra.printGeoPath(trg);
-                assert(dist==dist2);
-
-                src=generator.nextInt(ramGraph.nofNodes());
-                trg=generator.nextInt(ramGraph.nofNodes());
-            }
-            ramGraph=ramGraph2;
-
-            CHConstructor myCH=new CHConstructor(ramGraph);
+            CHConstructor myCH=new CHConstructor(prunedGraph);
             long overallTime=System.currentTimeMillis();
             long curTime, timeDelta;
 
             System.out.println("Starting Contraction!");
-            for(int k=0; k<300; k++)
+            for(int k=0;; k++)
             {
                 System.out.println("*************************************** "+(System.currentTimeMillis()-overallTime));
                 curTime=System.currentTimeMillis();
@@ -92,7 +137,7 @@ public class Main {
                 timeDelta=System.currentTimeMillis()-curTime;
                 System.out.println("Level "+k+" contraction was "+timeDelta+" having "+n+" nodes");
                 if (n<=1)
-                    k=Integer.MAX_VALUE-1;
+                    break;
             }
 
 
@@ -107,9 +152,6 @@ public class Main {
 
             graphCH.sanityCheck();
 
-
-            CHDijkstra myCHDijkstra=new CHDijkstra(graphCH);
-            myCHDijkstra.checkCHreqs();
             graphCH.setCHShortCuts();
             BufferedOutputStream ostream;
             if (!cmd.hasOption('o')){
@@ -121,41 +163,13 @@ public class Main {
                     ostream = new BufferedOutputStream(new FileOutputStream(cmd.getOptionValue('o')));
                 }
             }
-
-            graphCH.writeBIN(ostream);
-
-            myDijkstra=new Dijkstra(ramGraph);
-            int minDist=Integer.MAX_VALUE;
-
-            for(int i=0; i<100; i++)
-            {
-                //if ((graphCH.level(src)>0)&&(graphCH.level(trg)>0))
-                {
-                    curTime=System.nanoTime();
-                    int dist=myDijkstra.runDijkstra(graphCH.altNodeID(src),graphCH.altNodeID(trg));
-                    timeDelta=(System.nanoTime()-curTime)/1000;
-
-
-                    curTime=System.nanoTime();
-                    int dist2=myCHDijkstra.runDijkstra(src, trg);
-                    long timeDelta2=(System.nanoTime()-curTime)/1000;
-                    {
-                        minDist=dist;
-                        System.out.println("Distance from "+graphCH.altNodeID(src)+" to "+graphCH.altNodeID(trg)+" is "+dist+" in time "+timeDelta);
-                        System.out.println("CH-Distance in CH from "+src+" to "+trg+" is "+dist2+" in time "+timeDelta2);
-                        System.out.println("Levels are "+graphCH.level(src)+"/"+graphCH.level(trg));
-                        if (dist==Integer.MAX_VALUE)
-                            System.out.println("*******************************************************");
-                        // myDijkstra.printPath(trg);
-                        System.out.println();
-                        assert(dist==dist2);
-                    }
-
-                    src=generator.nextInt(ramGraph.nofNodes());
-                    trg=generator.nextInt(ramGraph.nofNodes());
-                }
-
+            if (cmd.hasOption("to")){
+                graphCH.writeGTXT(ostream);
+            } else {
+                graphCH.writeBIN(ostream);
             }
+
+            withChBench(prunedGraph, graphCH);
 
         } catch (FileNotFoundException fnf){
             System.err.println("Could not open the input file "+fnf.getMessage());
@@ -165,5 +179,7 @@ public class Main {
             System.err.println("Unexpected parse exception when reading command line "+ps.getMessage());
         }
     }
+
+
 
 }
