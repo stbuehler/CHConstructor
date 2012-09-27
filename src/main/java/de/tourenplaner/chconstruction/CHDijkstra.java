9678f76610bc71a42c509653f05afc8b18b83bfb
Peter Vollmer <vollmerpeter@googlemail.com>
2012-09-27 16:50:37 +0200

    Add license text

--- src/main/java/de/tourenplaner/chconstruction/CHDijkstra.java
+++ src/main/java/de/tourenplaner/chconstruction/CHDijkstra.java
@@ -1,3 +1,12 @@
+/*
+ * (C) Copyright 2012 Dr. Stefan Funke
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ */
+
 package de.tourenplaner.chconstruction;
 
 public class CHDijkstra extends BDDijkstra {
